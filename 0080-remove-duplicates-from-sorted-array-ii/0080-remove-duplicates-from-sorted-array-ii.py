a3ea661b4801e81775998eb3a5ea172c2abd582c
Msg05 <158449334+Msg05@users.noreply.github.com>
2025-08-17 01:18:09 +0530

    Time: 66 ms (79.7%), Space: 15.5 MB (16.61%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,15 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        class Solution(object):
-
         if len(nums) <= 2:
             return len(nums)
 
         i = 2  # start from index 2 since first two elements are always allowed
         for j in range(2, len(nums)):
-            if nums[j] != nums[i - 2]:  # checking for new element->i,i+1 will contain same element->so i-2=(i,i+1) going back to i -> that means if j is not equal to i and i+1, then put j in place of i
-                nums[i] = nums[j]       # place valid element
-                i += 1                  # move write pointer
+            # checking for new element
+            # nums[i-2] is the element two positions before i
+            # If nums[j] != nums[i-2], that means nums[j] is not a 3rd+ duplicate
+            if nums[j] != nums[i - 2]:
+                nums[i] = nums[j]   # copy nums[j] into the "write" spot
+                i += 1              # move the write pointer forward
 
-        return i  # length of valid part
+        return i  # i is the length of valid (deduplicated) part
